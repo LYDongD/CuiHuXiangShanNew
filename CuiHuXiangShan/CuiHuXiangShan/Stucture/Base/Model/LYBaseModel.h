caaefd627c8248d4850f9a5efc82ee30aa863b01
李悦东 <Lee@iMac.local>
2016-07-18 18:19:08 +0800

    new model

---
+++ CuiHuXiangShan/CuiHuXiangShan/Stucture/Base/Model/LYBaseModel.h
@@ -0,0 +1,13 @@
+//
+//  LYBaseModel.h
+//  CuiHuXiangShan
+//
+//  Created by 李悦东 on 16/7/18.
+//  Copyright © 2016年 fengdikeji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYBaseModel : NSObject
+
+@end
---
+++ CuiHuXiangShan/CuiHuXiangShan/Stucture/Base/Model/LYBaseModel.m
@@ -0,0 +1,13 @@
+//
+//  LYBaseModel.m
+//  CuiHuXiangShan
+//
+//  Created by 李悦东 on 16/7/18.
+//  Copyright © 2016年 fengdikeji. All rights reserved.
+//
+
+#import "LYBaseModel.h"
+
+@implementation LYBaseModel
+
+@end
